4e06282fb0ed6b146d0dda820e15b2933c99afb0
lfaure75 <febus95@hotmail.com>
2024-10-16 13:26:35 +0200

    Remove default app and add host app with NX react / webpack

--- apps/react-host/jest.config.ts
+++ apps/react-host/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
-  displayName: 'nx-react-workspace',
+  displayName: 'react-host',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/nx-react-workspace',
+  coverageDirectory: '../../coverage/apps/react-host',
 };
